f12f1ae48568016fa9ef400d067701e2ad95aead
gizem-bicer <gizem.bicer@accenture.com>
2026-06-08 20:48:33 +0200

    FIX further improved detecting Chrome errors (#116)
    NEW chrome info for features
    FIX errors occur after button click closed

--- Install/Sql/MsSQL/Migrations/1.2/20260608_1505_1_NEW_feature_info_column.sql
+++ Install/Sql/MsSQL/Migrations/1.2/20260608_1505_1_NEW_feature_info_column.sql
@@ -1,8 +1,8 @@
 -- UP
 
-ALTER TABLE bdt_run
+ALTER TABLE bdt_run_feature
     ADD chrome_info NVARCHAR(MAX) NULL;
 -- DOWN
 
-ALTER TABLE bdt_run
+ALTER TABLE bdt_run_feature
 DROP COLUMN chrome_info;
