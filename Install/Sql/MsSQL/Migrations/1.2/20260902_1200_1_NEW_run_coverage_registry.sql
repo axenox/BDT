a744a60f84a97a164b209134de2df9b99d6b222b
gizem-bicer <gizem.bicer@accenture.com>
2026-09-04 20:17:40 +0200

    FIX sql tables and object

--- Install/Sql/MsSQL/Migrations/1.2/20260902_1200_1_NEW_run_coverage_registry.sql
+++ Install/Sql/MsSQL/Migrations/1.2/20260902_1200_1_NEW_run_coverage_registry.sql
@@ -1,36 +1,30 @@
--- UP
-
 IF OBJECT_ID(N'dbo.bdt_run_coverage_registry', N'U') IS NULL
 BEGIN
-    CREATE TABLE dbo.bdt_run_coverage_registry
-    (
-        oid                  binary(16)    NOT NULL,
-        created_on           datetime      NOT NULL,
-        modified_on          datetime      NOT NULL,
-        created_by_user_oid  binary(16)    NOT NULL,
-        modified_by_user_oid binary(16)    NOT NULL,
-        run_uid              varchar(32)   NOT NULL,
-        screen_slug          nvarchar(160) NOT NULL,
-        screen_kind          varchar(10)   NOT NULL,
-        widget_id            nvarchar(160) NOT NULL,
-        object_uid           varchar(34)   NOT NULL,
-        role_key             nvarchar(100) NOT NULL,
-        work_category        varchar(50)   NOT NULL,
-        element              nvarchar(100) NOT NULL,
-        action_fingerprint   char(64)      NOT NULL,
-        status               int           NOT NULL,
-        started_on           datetime      NOT NULL,
-        finished_on          datetime      NULL,
+CREATE TABLE dbo.bdt_run_coverage_registry (
+        oid                     binary(16)      NOT NULL,
+        created_on              datetime2(0)    NOT NULL,
+        modified_on             datetime2(0)    NOT NULL,
+        created_by_user_oid     binary(16)      NOT NULL,
+        modified_by_user_oid    binary(16)      NOT NULL,
+        run_uid                 binary(16)      NOT NULL,
+        screen_slug             nvarchar(160)   NOT NULL,
+        screen_kind             nvarchar(10)    NOT NULL,
+        widget_id               nvarchar(160)   NOT NULL,
+        object_uid              nvarchar(34)    NOT NULL,
+        role_key                nvarchar(400)   NOT NULL,
+        work_category           nvarchar(50)    NOT NULL,
+        element                 nvarchar(160)   NOT NULL,
+        action_fingerprint      char(64)        NOT NULL,
+        identity_hash           char(64)        NOT NULL,
+        status                  int             NOT NULL,
+        started_on              datetime2(0)    NOT NULL,
+        finished_on             datetime2(0)    NULL,
+        CONSTRAINT PK_run_coverage_registry PRIMARY KEY CLUSTERED (oid),
+        CONSTRAINT CK_run_coverage_registry_screen_kind
+            CHECK (screen_kind IN (N'page', N'dialog', N'popup')),
+        CONSTRAINT UQ_run_coverage_registry_identity UNIQUE NONCLUSTERED (identity_hash)
+);
 
-        CONSTRAINT PK_bdt_run_coverage_registry PRIMARY KEY CLUSTERED (oid),
-        CONSTRAINT CK_bdt_run_coverage_registry_screen_kind CHECK (screen_kind IN ('page', 'dialog', 'popup')),
-        CONSTRAINT CK_bdt_run_coverage_registry_work_category CHECK (work_category IN ('Filtering', 'Buttons')),
-        CONSTRAINT UQ_bdt_run_coverage_registry_identity UNIQUE NONCLUSTERED
-            (run_uid, screen_slug, screen_kind, widget_id, object_uid, role_key, work_category, element, action_fingerprint)
-    );
+CREATE NONCLUSTERED INDEX IX_run_coverage_registry_run
+        ON dbo.bdt_run_coverage_registry (run_uid);
 END
-GO
-
--- DOWN
-
--- Do not delete tables!
